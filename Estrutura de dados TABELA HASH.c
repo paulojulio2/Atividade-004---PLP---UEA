9dc4aee2d67a38fd3feee9a746d299d3ff68ef2d
paulojulio2 <120578144+paulojulio2@users.noreply.github.com>
2023-01-25 15:20:54 -0400

    Update Estrutura de dados TABELA HASH.c

--- Estrutura de dados TABELA HASH.c
+++ Estrutura de dados TABELA HASH.c
@@ -3,26 +3,18 @@
 
 int tamanhoTabela = 10;
 
-/*Rotina que transforma uma string em um nÃºmero
-que serÃ¡ usado depois na funÃ§Ã£o hash
-*/
+
 int stringParaInt(char *string) {
-    int tamanho, primeira, segunda; //Inteiros que representam o tamanho,
-                                    //o cÃ³digo da primeira letra da string
-                                    //e o cÃ³digo da segunda letra.
-    tamanho =  strlen(string);      //Mede o tamanho da string
-    primeira = string[0];           //ObtÃ©m o cÃ³digo da primeira letra
-    segunda = string[1];            //ObtÃ©m o cÃ³digo da segunda letra
-    int resultado = (tamanho * primeira) + segunda; //FunÃ§Ã£o de transformaÃ§Ã£o
+    int tamanho, primeira, segunda; 
+                                 
+    tamanho =  strlen(string);      
+    primeira = string[0];           
+    segunda = string[1];            
+    int resultado = (tamanho * primeira) + segunda;
     return resultado;  //Retorna nÃºmero que representa a string
     }
 
-/*A funÃ§Ã£o mais simples de hash;
-Para uma tabela com n posiÃ§Ãµes (n == tamanhoTabela)
-Toma-se o mÃ³dulo n do valor inteiro gerado na
-funÃ§Ã£o "stringParaInt"
-RetornarÃ¡ um nÃºmero entre 0 e 19.
-*/
+
 int hash(int valor) {
     return valor % tamanhoTabela;  
     }
@@ -43,6 +35,6 @@ int main() {
     printf("\nDigite uma palavra qualquer: ");
     gets(dado);
 
-    printf("A chave para a tabela (de 0 a %d) Ã©: %d", tamanhoTabela-1, hash(stringParaInt(dado)));
+    printf("A chave para a tabela (de 0 a %d): %d", tamanhoTabela-1, hash(stringParaInt(dado)));
     }
 
